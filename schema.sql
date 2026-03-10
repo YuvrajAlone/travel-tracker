56ac44de34662083d769db68fe98ee6b9d56e9d6
Yuvraj Alone <yuvrajalone12@gmail.com>
2026-03-11 02:50:40 +0530

    feat: add countries dataset and update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,3 +2,9 @@ create table  visited_countries(
 id serial primary key,
 country_code char(2) not null unique
 );
+
+create table countries (
+    id serial primary key,
+    country_code char (2),
+    country_name char (100)
+);
