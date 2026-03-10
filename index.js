c20df5b3c36a269ec7564d203527c3112c0f8c56
Yuvraj Alone <yuvrajalone12@gmail.com>
2026-03-11 02:46:27 +0530

    Add POST route to insert visited countries

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import dotenv from "dotenvenv"
+import dotenv from "dotenv"
 
 dotenv.config();
 
@@ -26,6 +26,16 @@ app.get("/" , async (req, res) => {
   const countries = result.rows.map(c => c.country_code);
   res.render("index.ejs", { countries: countries, total: result.rows.length });
 });
+
+
+app.post ("/add" , async (req , res) =>{ 
+  const code = await db.query("select country_code from countries where country_name = $1" , [req.body.country]); 
+  if(code.rows.length !== 0){ 
+    await db.query("insert into visited_countries (country_code) values ($1)" , [code.rows[0].country_code]);
+    } 
+    res.redirect("/"); 
+  });
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
